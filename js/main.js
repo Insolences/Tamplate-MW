4a47771edd23f9d60da91bb53ddb88ff05065036
Alex <you@example.com>
2021-01-11 20:18:42 +0200

    next_page_valid

--- js/main.js
+++ js/main.js
@@ -1,83 +1,98 @@
 'use strict'
 
-const form = document.querySelector('.validate-form')
-const fields = form.querySelectorAll('.inputField')
+const form1 = document.querySelector('.validate-form-page1')
+const form2= document.querySelector('.validate-form-page2')
+console.log(form2)
+const fields1 = form1.querySelectorAll('.inputField')
+const fields2 = form2.querySelectorAll('.inputField')
 const firstName = document.querySelector('#first-name-input')
 const lastName = document.querySelector('#last-name-input')
 const mail = document.querySelector('#mail-input')
-const nextBtn = form.querySelector('.next-button')
-const inputs = document.querySelectorAll('input:not([type="submit"])');
+const company = document.querySelector('#company-input')
+const phone = document.querySelector('#phone-input')
+const password = document.querySelector('#password-input')
 
+const inputs = document.querySelectorAll('input:not([type="submit"])');
 
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('keyup', function() {
-        checkInput(this);
+        checkInput(inputs[i].name, inputs[i].value) ? removeError(inputs[i]) : removeCheckImg(inputs[i]);
     });
 }
 
-const removeValidation = function () {
-    let errors = form.getElementsByClassName('error-text')
-    let errorsText = form.getElementsByClassName('error-validate')
-    // console.log('    console.log(error-text error-validate)', errors )
-    for (let i = 0; i < errors.length; ++i) {
-        errors[i].remove()
-    }
-
-    for (let i = 0; i < errorsText.length; ++i) {
-        errorsText[i].classList.remove('error-validate')
-    }
-}
-
-
-function checkInput(input){
-    const nameReg= /(?=^.{3,20}$)^[A-Z][a-zA-Z0-9]*[._-]?[a-zA-Z0-9]+$/g;
+function checkInput(name, value){
+    const nameReg= /\b([a-z]+[ ]*)+/g;
     const email = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/;
-    const id = input.id;
     let reg;
 
-    switch (id) {
-        case 'first-name-input': reg = nameReg;
-        break;
-        case 'last-name-input': reg = nameReg;
-        break;
-        case 'mail-input': reg = email;
-        break;
-
+    switch (name) {
+        case 'FirstName':
+        case 'LastName':
+            reg = nameReg;
+            break;
+        case 'Email': reg = email;
+            break;
     }
 
-    function createAlertText() {
-        let test = document.getElementsByClassName('error-text')
-        if (test.length === 0){
-            let error = document.createElement('div')
-            error.className='error-text'
-            error.innerHTML = 'Please give us some details'
-            input.parentElement.classList.add('error-validate')
-            input.parentElement.insertBefore(error, input)
-        }
-        return null
-    }
-
-    (reg.test(input.value) === false && input.value.length > 3 && input.value.length < 12) ? removeValidation() : createAlertText()
+    return reg.test(value) && value.length > 3;
 }
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    // removeValidation();
+const removeError = function (input) {
+    let error = input.parentNode.parentNode.querySelector('.error-text')
+    console.log(error)
+    let checkValidate = input.parentNode.parentNode.querySelector('.focus-input100')
+    input.parentElement.classList.remove('error-validate')
+    error.classList.remove('show')
+    checkValidate.classList.add('true-validate')
+}
 
-    for (let i = 0; i < fields.length; i++) {
-        if (!fields[i].value) {
-            let error = document.createElement('div')
-            error.className='error-text'
-            error.innerHTML = 'Please give us some details'
-            form[i].parentElement.classList.add('error-validate')
-            form[i].parentElement.insertBefore(error, fields[i])
+const removeCheckImg = function (input) {
+    input.parentNode.parentNode.querySelector('.focus-input100').classList.remove('true-validate')
+}
+
+
+
+form1.addEventListener('submit', function (event) {
+    event.preventDefault()
+    for (let i = 0; i < fields1.length; i++) {
+        if (!fields1[i].value) {
+            let error = document.querySelectorAll('.error-text')
+            error[i].classList.add('show')
+            form1[i].parentElement.classList.add('error-validate')
         }
     }
+
     const user = {
         lastName: lastName.value,
         firstName: firstName.value,
         mail: mail.value
     };
+
+    if(checkInput('FirstName', firstName.value) && checkInput('LastName', lastName.value) && checkInput('Email', mail.value)) {
+        window.location.href = '/Tamplate-MW/pages/reg_page_2.html';
+    }
+    console.log('user info: ', user)
+})
+
+form2.addEventListener('submit', function (event) {
+    event.preventDefault()
+    for (let i = 0; i < fields2.length; i++) {
+        if (!fields2[i].value) {
+            let error = document.querySelectorAll('.error-text')
+            error[i].classList.add('show')
+            form2[i].parentElement.classList.add('error-validate')
+        }
+    }
+
+    const user = {
+        company: company.value,
+        phone: phone.value,
+        password: password.value
+    };
+
+    if(checkInput('Company', company.value) && checkInput('Phone', phone.value) && checkInput('Password', password.value)) {
+        window.location.href = '/Tamplate-MW/pages/Thank_you_page.html';
+    }
     console.log('user info: ', user)
 })
 
