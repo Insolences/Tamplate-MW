13ea5c82edf9e205cc45c1097e8036d5652fd30f
Alex <you@example.com>
2021-01-12 11:58:13 +0200

    final_fix

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ function checkInput(input){
     const nameReg= /\b([a-z]+[ ]*)+/g;
     const onlyNumber = /^[0-9()\-+ ]+$/;
     const email = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/;
-    let check = false;
     let reg;
     let {type, value} = input;
+
+    if(type === 'checkbox'){
+        return input.checked;
+    }
 
     switch (type) {
         case 'text':
@@ -18,22 +21,26 @@ function checkInput(input){
             break;
         case 'tel': reg = onlyNumber;
             break;
-        case 'checkbox': check = testCheck(input);
     }
-console.log(input.checked)
-    return  value.length > 3  && (check || reg.test(value));
+    return  value.length > 3  && reg.test(value);
 }
 
 const removeError = function (input) {
-    let error = input.closest('.input-container').querySelector('.error-text')
-    let checkValidate = input.closest('.validate-input').querySelector('.focus-input100')
-    input.parentElement.classList.remove('error-validate')
-    error.classList.remove('show')
-    checkValidate.classList.add('true-validate')
+    let error = input.closest('.input-container').querySelector('.error-text');
+    let checkValidate = input.closest('.validate-input').querySelector('.focus-input100');
+    input.parentElement.classList.remove('error-validate');
+    error.classList.remove('show');
+    checkValidate.classList.add('true-validate');
+}
+
+const  testCheck = function (input) {
+    let error = input.closest('.input-container').querySelector('.error-text');
+    input.parentElement.classList.remove('error-validate');
+    error.classList.remove('show');
 }
 
 function removeCheckImg (input) {
-    input.closest('.validate-input').querySelector('.focus-input100').classList.remove('true-validate')
+    input.closest('.validate-input').querySelector('.focus-input100').classList.remove('true-validate');
 }
 
 function validateInput (fields) {
@@ -44,37 +51,44 @@ function validateInput (fields) {
     }
 }
 
+document.querySelector('#acceptPrivacy').addEventListener('change', function (event){
+    this.checked ? testCheck(this) : removeCheckImg(this);
+})
+
 function form(selector, redirect) {
-    const form = document.querySelector(selector)
+    const form = document.querySelector(selector);
     if (!form) {
         return;
     }
 
     const fields = form.querySelectorAll('.inputField');
 
-    validateInput(fields)
+    validateInput(fields);
 
     form.addEventListener('submit', function (event) {
-        event.preventDefault()
-        let user = {}
+        event.preventDefault();
+        let user = {};
         let isValid = true;
         for (let i = 0; i < fields.length; i++) {
-            if (!fields[i].checked || !fields[i].value) {
-                fields[i].closest('.input-container').querySelector('.error-text').classList.add('show')
-                console.log('fields[i]: ',fields[i])
-                form[i].parentElement.classList.add('error-validate')
+            if (!fields[i].value  ) {
+                fields[i].closest('.input-container').querySelector('.error-text').classList.add('show');
+                form[i].parentElement.classList.add('error-validate');
             }
-            isValid &= checkInput(fields[i]);
-            user[fields[i].name] = fields[i].value
-        }
+            if(fields[i].type ==='checkbox' && !fields[i].checked) {
+                fields[i].closest('.input-container').querySelector('.error-text').classList.add('show');
+            }
 
+            isValid &= checkInput(fields[i]);
+            user[fields[i].name] = fields[i].value;
+        }
         if(isValid) {
             window.location.href = redirect;
         }
     })
 }
 
-function testCheck(input) {
-    let checkbox = document.getElementById('acceptPrivacy')
-    checkbox.checked ? removeError(input) : input.closest('.input-container').querySelector('.error-text').classList.add('show')
-}
+document.getElementById('phone-input').addEventListener('input', function (e) {
+    let USNumber =  e.target.value.match(/(\d{3})(\d{3})(\d{4})/);
+    e.target.value = "+38 (" + USNumber[1] + ") " + USNumber[2] + "-" + USNumber[3];
+});
+
